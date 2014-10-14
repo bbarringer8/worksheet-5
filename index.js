dce5bfb4a06c350a442d82a931a1554464b3bd6c
bbarringer8 <bbarring@nebrwesleyan.edu>
2014-10-14 14:14:21 -0400

    Added Solution for Problem 5

--- index.js
+++ index.js
@@ -2,3 +2,57 @@
 /*jslint browser: true*/ // This line tells jsLint that the code will run in a browser.
 /*global console*/ // This line tells jsLint that you're allowed to use console.log for printing.
 
+function minimum_of_two(first_number, second_number) {
+var minimum;
+if (first_number < second_number) {
+minimum = first_number;
+}
+else {
+minimum = second_number;
+}
+return minimum;
+}
+function coin_flip() {
+var heads_or_tails;
+if (Math.random() < 0.5) {
+heads_or_tails = 'heads';
+}
+else {
+heads_or_tails = 'tails';
+}
+return heads_or_tails;
+}
+function six_sided_dice() {
+var dice = Math.ceil(Math.random() * 6);
+return dice;
+}
+// Problem 3
+function five_rolls() {
+var list = [];
+var i = 0;
+while (i < 5) {
+list[i] = six_sided_dice();
+i += 1;
+}
+return list;
+}
+function Yahtzee(list) {
+var i = 0;
+while (i < 4) {
+if( list [i] !== list [ i + 1]){
+return 0; }
+i = i + 1;
+}
+return 50;
+}
+function total(arbitraryList) {
+	var sum=0;
+	var current;
+	current=0;
+	while (current !== arbitraryList.length){
+		sum=arbitraryList[current]+sum;
+		current=current+1;
+		}
+	return sum;
+}
+
